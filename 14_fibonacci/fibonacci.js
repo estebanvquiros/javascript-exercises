cb3b5c2d8afadd30339dcee7f6f4f948e1a489e4
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-10-07 20:09:21 -0600

    Add function to calculate fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    let n = typeof num !== 'number' ? parseInt(num, 10) : num; 
+
+    if (n < 0) return "OOPS";
+
+    let a = 0; //F(0)
+    let b = 1; //F(1)
+    
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return n === 0 ? a : b;
 
 };
 
