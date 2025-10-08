ed67ce8a98d7b108251f9d98f0c0dbc1cf6b0502
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-10-07 18:56:16 -0600

    Add function to check palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let normalizedString = string.toLowerCase().replace(/[!.,\s]/g, '');
+    let reversedString = normalizedString.split('').reverse().join('');
+    return normalizedString === reversedString;
 };
 
 // Do not edit below this line
