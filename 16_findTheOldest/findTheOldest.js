0e9df168dd239b29d51e1fd3bf76a665776b3a61
Esteban Vargas Quirós <79805608+estebanvquiros@users.noreply.github.com>
2025-10-08 12:22:35 -0600

    Add function to get the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    return !death
+        ? new Date().getFullYear() - birth
+        : death - birth;
+}
 
+const findTheOldest = function(people) {
+
+    return people.reduce((oldest, current) => {
+
+        oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+        return currentAge > oldestAge ? current : oldest;
+
+    });
+    
 };
 
 // Do not edit below this line
